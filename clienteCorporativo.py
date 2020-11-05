921e45d74e8f7b931552d299893c2a096d71961c
bruno_ballinari <brunoballinari@gmail.com>
2020-11-05 04:03:15 -0300

    nueva actualizacion

--- clienteCorporativo.py
+++ clienteCorporativo.py
@@ -10,3 +10,9 @@ class ClienteCorporativo(Cliente):
         self.telefono_contacto = telefono_contacto
         super().__init__(telefono, mail, id_cliente)
          
+    def __str__(self):
+        Cadena = f"ID cliente: {self.id_cliente}\n"
+        Cadena += f"{self.nombre_empresa} (Cliente Corporativo)\n"
+        Cadena += f"Tel.Corporativo: {self.telefono} - Email: {self.mail}\n"
+        Cadena += f"Nombre de Contacto: {self.nombre_contacto} - Tel.Contacto: {self.telefono_contacto}\n"
+        return Cadena
