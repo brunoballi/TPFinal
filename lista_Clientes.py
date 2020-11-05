633234e6ae54ab8ab70cfd518e7c70a02c308caf
bruno_ballinari <brunoballinari@gmail.com>
2020-11-05 05:11:19 -0300

    modificar datos clientes

--- lista_Clientes.py
+++ lista_Clientes.py
@@ -45,7 +45,55 @@ class ListaClientes:
                 return (C)
         return None
 
+    def modificar_datos_CP(self, nombre, apellido, telefono, mail, id_cliente):
+        """Recibe los datos modificados y actualiza los datos del cliente particular"""
+        Cp = self._buscar_por_id(id_cliente)
+        if Cp:
+            if nombre == '':
+                nombre = Cp.nombre
+            else:
+                Cp.nombre = nombre
+            if apellido == '':
+                apellido = Cp.apellido
+            else:
+                Cp.apellido = apellido
+            if telefono == '':
+                telefono = Cp.telefono
+            else:
+                Cp.telefono = telefono
+            if mail == '':
+                mail = Cp.mail
+            else:
+                Cp.mail = mail
+            return self.rc.update(Cp)
+        return None
 
+    def modificar_datos_CC(self, nombre_empresa, nombre_contacto, telefono_contacto, telefono, mail, id_cliente):
+        """Recibe los datos modificados y actualiza los datos del cliente corporativo"""
+        Cc = self._buscar_por_id(id_cliente)
+        if Cc:
+            if nombre_empresa == '':
+                nombre_empresa = Cc.nombre_empresa
+            else:
+                Cc.nombre_empresa = nombre_empresa
+            if nombre_contacto == '':
+                nombre_contacto = Cc.nombre_contacto
+            else:
+                Cc.nombre_contacto = nombre_contacto
+            if telefono_contacto == '':
+                telefono_contacto = Cc.telefono_contacto
+            else:
+                Cc.telefono_contacto = telefono_contacto
+            if telefono == '':
+                telefono = Cc.telefono
+            else:
+                Cc.telefono = telefono
+            if mail == '':
+                mail = Cc.mail
+            else:
+                Cc.mail = mail
+            return self.rc.update(Cc)
+        return None
 
 
 
